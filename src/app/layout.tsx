a5226a95fdd2128a4131f158578ff035150159da
Anurag Lodhi <anuraglodhi63@gmail.com>
2024-07-17 15:43:40 +0530

    feat: added dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/components/providers/react-query-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,12 +25,20 @@ export default function RootLayout({
             inter.className + "flex h-screen w-screen overflow-x-hidden"
           }
         >
-          <div className="flex h-full w-full flex-col items-center">
-            <header className="flex h-[100px] w-full flex-shrink-0 items-center px-8">
-              <h1 className="text-2xl font-semibold">YT Live views</h1>
-            </header>
-            {children}
-          </div>
+          <ThemeProvider
+            defaultTheme="system"
+            attribute="class"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex h-full w-full flex-col items-center">
+              <header className="flex h-[100px] w-full flex-shrink-0 items-center px-8 justify-between">
+                <h1 className="text-2xl font-semibold">YT Live views</h1>
+                <ModeToggle />
+              </header>
+              {children}
+            </div>
+          </ThemeProvider>
         </body>
       </QueryProvider>
     </html>
